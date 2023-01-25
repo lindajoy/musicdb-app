a8e05019d6cd1bb412daa88246e7f6424573aeba
Linda Joy <wawirajoy98@gmail.com>
2023-01-25 14:45:10 +0300

    ✏️ Created application for the artists home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ArtistsHomePageComponent } from './components/musicHome/artists.home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtistsHomePageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/musicHome/artists.home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'deezer-home-component',
+    templateUrl: 'artists.home.component.html',
+    styleUrls: ['artists.home.component.scss']
+})
+
+export class ArtistsHomePageComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
