91fac5db6359f7b902da822d8b0032a542063026
Linda Joy <wawirajoy98@gmail.com>
2023-01-28 15:56:01 +0300

    ✏️ Added the first view of the artists home page

--- src/app/components/musicHome/artists.home.component.ts
+++ src/app/components/musicHome/artists.home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl } from '@angular/forms';
-import { debounceTime,  Subscription } from 'rxjs';
+import { catchError, debounceTime,  of,  Subscription } from 'rxjs';
 import { Artist } from 'src/app/interfaces/artist.interface';
 import { MusicService } from 'src/app/services/deezer.service';
 
@@ -14,7 +14,7 @@ export class ArtistsHomePageComponent implements OnInit {
     
     searchControl!: FormControl;
     searchTextformCtrlSub!: Subscription;
-    artist:any;
+    artist!:Artist;
     
     constructor(private _formBuilder: FormBuilder,
                 private _musicService: MusicService) { }
@@ -35,10 +35,25 @@ export class ArtistsHomePageComponent implements OnInit {
     // This function will search for the artist and display the artist on screen.
     searchArtist(artist: string)
     {
-      return this._musicService.SearchArtistByName(artist)
-                 .subscribe(artists => {console.log('😁',artists)});
+      return this._musicService
+                  .SearchArtistByName(artist)
+                  .pipe(
+                    // catchError operator takes as input an Observable that might error out.
+                    catchError(err => of([])))
+                 .subscribe((artist) => {
+                   this.createArtistObject(artist);
+                 });
     }
 
 
+    createArtistObject(artist: any)
+    {
+      this.artist = {
+            name: artist?.data[0]?.artist?.name,
+            picture: artist?.data[0]?.artist?.picture_medium,
+            fans: artist?.total
+      } as Artist
 
+      return this.artist;
+    }
 }        
