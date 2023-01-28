9dbc5b7d433ceee967051f98cc523f4c865f3904
Linda Joy <wawirajoy98@gmail.com>
2023-01-28 12:06:53 +0300

    📦 Added relevant modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { ArtistsHomePageComponent } from './components/musicHome/artists.home.component';
+import { MusicService } from './services/deezer.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,9 +19,15 @@ import { ArtistsHomePageComponent } from './components/musicHome/artists.home.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule
   ],
-  providers: [],
+  providers: [ MusicService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
