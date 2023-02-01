97c2264b26ed9b735ec825df255cad71f6f6292e
Linda Joy <wawirajoy98@gmail.com>
2023-02-01 06:17:33 +0300

    ✏️ Added id on the interface artist

--- src/app/interfaces/artist.interface.ts
+++ src/app/interfaces/artist.interface.ts
@@ -1,4 +1,5 @@
 export interface Artist {
+    id: string // unique id
     name: string, // This is the name of the artist
     picture: string, // This is the URL to the photo
     fans: number // Number of fans
