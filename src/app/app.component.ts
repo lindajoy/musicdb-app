a2ee48b57ec7bde9bf00f323076b7f8c19d0d504
Linda Joy <wawirajoy98@gmail.com>
2023-02-01 06:27:14 +0300

    ❌ Removed unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -13,8 +12,7 @@ export class AppComponent implements OnInit {
   header!: string
 
 
-  constructor(private router: Router,
-              private _activatedRoute: ActivatedRoute){}
+  constructor(private router: Router){}
 
   ngOnInit()
   {
