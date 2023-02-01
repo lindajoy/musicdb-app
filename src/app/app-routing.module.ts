4dcca2525ae4afb8415104313c6daef7c88a9884
Linda Joy <wawirajoy98@gmail.com>
2023-02-01 06:17:08 +0300

    📜 Added proper comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { DetailArtistsComponent } from './components/detail-artists/detail-artists.component';
 import { ArtistsHomePageComponent } from './components/musicHome/artists.home.component';
 
+
+// Added dynamic page titles as per this article: 
+// Ref: https://dev.to/brandontroberts/setting-page-titles-natively-with-the-angular-router-393j#:~:text=First%2C%20you%20would%20use%20the,the%20title%20for%20the%20page.&text=Next%2C%20you%20would%20add%20code,to%20set%20the%20page%20title.
+
 const routes: Routes = [
   {
     path: '',
-    component: ArtistsHomePageComponent
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path:'home',
+    component: ArtistsHomePageComponent,
+    title: "Deezer Home Page"
   },
   {
     path: 'artist-details',
-    component:  DetailArtistsComponent 
+    component:  DetailArtistsComponent,
+    title: 'Artist Details Page'
   }
 ]
 @NgModule({
