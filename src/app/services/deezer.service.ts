1c0f90ce70ad603f4a815561d2cc936e6d214a80
Linda Joy <wawirajoy98@gmail.com>
2023-02-04 23:55:47 +0300

    🛠 Added methods

--- src/app/services/deezer.service.ts
+++ src/app/services/deezer.service.ts
@@ -6,11 +6,23 @@ import { HttpClient } from '@angular/common/http';
 export class MusicService {
 
     searchUrl: string = "search?q=";
-    
+    artistDetailsUrl: string = "artist/";
+    trailingUlr: string = "/top?limit=10";
+
     constructor(private httpClient: HttpClient) { }
 
     searchArtistByName(artistName: string)
     {
      return this.httpClient.get(this.searchUrl + artistName);
+    }
+
+    getArtistById(artistId: number)
+    {
+      return this.httpClient.get(this.artistDetailsUrl + artistId);
+    }
+
+    getAristsTopTracks(artistId: string)
+    {
+     return this.httpClient.get(this.artistDetailsUrl + artistId+ this.trailingUlr );
     }
 }
